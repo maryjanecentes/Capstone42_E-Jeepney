5bddbdfb50746c10737e21e0eb2bd788cd08b8b8
Mary Jane Centes <37787796+maryjanecentes@users.noreply.github.com>
2018-04-03 07:28:03 +0800

    Add files via upload

---
+++ ARDUINO1/ARDUINO1.ino
@@ -0,0 +1,232 @@
+#include <Wire.h>
+
+//declaration of constant variables
+
+const int studentButton = 3;
+const int seniorpwdButton = 4;
+const int regularButton = 5;
+const int nocoinsButton = 6;
+const int yesButton = 7;
+const int noButton = 8;
+const int openButton = 9;
+const int clearButton = 10;
+const int buzzerPin = 11;
+
+const int studentFare = 1;
+const int seniorpwdFare = 2;
+const int regularFare = 3;
+const int openLock = 4;
+const int delayValue = 5;
+const int confirmation = 6;
+const int requestOpen = 7;
+const int clearCounter = 8;
+
+//declaration of non constant variables
+
+int studentButtonState = 0;
+int seniorpwdButtonState = 0;
+int regularButtonState = 0;
+int nocoinsButtonState = 0;
+int yesButtonState = 0;
+int noButtonState = 0;
+int openButtonState = 0;
+int clearButtonState = 0;
+int lastButtonState = 0;
+int tempFare = 0;
+int counter = 0;
+
+//setting up all input/output pins
+void setup()
+{
+
+  pinMode(studentButton, INPUT);
+  pinMode(seniorpwdButton, INPUT);
+  pinMode(regularButton, INPUT);
+  pinMode(nocoinsButton, INPUT);
+  pinMode(yesButton, INPUT);
+  pinMode(noButton, INPUT);
+  pinMode(openButton, INPUT);
+  pinMode(clearButton, INPUT);
+  pinMode(buzzerPin, OUTPUT);
+
+  digitalWrite(buzzerPin,HIGH);
+
+  Serial.begin(9600);
+  Wire.begin();
+}
+
+void loop()
+{
+  //reading of inputs
+  
+  studentButtonState = digitalRead(studentButton);
+  seniorpwdButtonState = digitalRead(seniorpwdButton);
+  regularButtonState = digitalRead(regularButton);
+
+  nocoinsButtonState = digitalRead(nocoinsButton);  
+  openButtonState = digitalRead(openButton);
+  clearButtonState = digitalRead(clearButton);
+
+
+  if(studentButtonState != lastButtonState)//if student button is pressed
+  {
+    if(studentButtonState == HIGH)
+    {
+      //turning on the buzzer for 1 sec
+      digitalWrite(buzzerPin, LOW);
+      delay(1000);
+      digitalWrite(buzzerPin,HIGH);
+
+      Wire.beginTransmission(8);
+      Wire.write(confirmation);
+      Wire.endTransmission();
+
+      while(counter <= 1000){
+
+        noButtonState = digitalRead(noButton);
+        yesButtonState = digitalRead(yesButton);
+
+        counter = counter + 1;
+        
+        //confirmation if passenger is discounted
+        if(yesButtonState == HIGH)
+        {
+          Wire.beginTransmission(8);
+          Wire.write(studentFare);//transmitting the signal of the variable studentFare
+          Wire.endTransmission();
+          break;
+        }
+        else if(noButtonState == HIGH)
+        {
+          Wire.beginTransmission(8);
+          Wire.write(regularFare);//transmitting the signal of the variable regularFare
+          Wire.endTransmission();
+          break;
+        }
+        else if(counter == 1000)
+        {
+          Wire.beginTransmission(8);
+          Wire.write(delayValue);
+          Wire.endTransmission();
+          break;
+        }
+      }
+    }
+    lastButtonState = studentButtonState;
+  }
+  else if (seniorpwdButtonState != lastButtonState)//if senior/pwd button is pressed
+  {
+    if(seniorpwdButtonState == HIGH)
+    {
+      digitalWrite(buzzerPin,LOW);
+      delay(1000);
+      digitalWrite(buzzerPin,HIGH);
+      Wire.beginTransmission(8);
+      Wire.write(confirmation);
+      Wire.endTransmission();
+
+
+      while(counter <= 1000){
+
+        noButtonState = digitalRead(noButton);
+        yesButtonState = digitalRead(yesButton);
+
+        counter = counter + 1;
+        //confirmation if the passenger is discounted
+        if(yesButtonState == HIGH)
+        {
+
+          Wire.beginTransmission(8);
+          Wire.write(seniorpwdFare);//transmitting the signal of the variable seniorpwdFare
+          Wire.endTransmission();
+          break;
+
+        }
+        else if(noButtonState == HIGH)
+        {
+
+          Wire.beginTransmission(8);
+          Wire.write(regularFare);//transmitting the signal of the variable regularFare
+          Wire.endTransmission();
+          break; 
+        }
+        else if(counter == 1000)
+        {
+          Wire.beginTransmission(8);
+          Wire.write(delayValue);
+          Wire.endTransmission();
+          break;
+        }
+
+      }
+    }
+    lastButtonState = seniorpwdButtonState;
+  }
+  else if (regularButtonState != lastButtonState)//if regular button is pressed
+  {
+    if(regularButtonState == HIGH)
+    {
+
+      Wire.beginTransmission(8);
+      Wire.write(regularFare);//transmitting the signal of the variable regularFare
+      Wire.endTransmission();
+      delay(500);
+    }
+
+  }
+  else if (nocoinsButtonState != lastButtonState)//if no coins button is pressed
+  {
+    if(nocoinsButtonState == HIGH)
+    {
+      //turning on the buzzer for 1 sec
+      digitalWrite(buzzerPin,LOW);
+      delay(1000);
+      digitalWrite(buzzerPin,HIGH);
+
+      Wire.beginTransmission(8);
+      Wire.write(requestOpen);//transmitting the signal of the variable requestOpen
+      Wire.endTransmission();
+      delay(500);
+
+    }
+    lastButtonState = nocoinsButtonState;
+  }
+  else if (openButtonState != lastButtonState)//if open lock button is pressed
+  {
+    if(openButtonState == HIGH)
+    {
+      Wire.beginTransmission(8);
+      Wire.write(openLock);//transmitting the signal of openLock
+      Wire.endTransmission();
+      delay(500);
+    }
+    lastButtonState = openButtonState;
+  }
+  else if (clearButtonState != lastButtonState)//if clear button is pressed
+  {
+    if(clearButtonState == HIGH)
+    {
+      Wire.beginTransmission(8);
+      Wire.write(clearCounter);//transmitting the signal of clearCounter
+      Wire.endTransmission();
+      delay(500);
+
+    }
+    lastButtonState = clearButtonState;
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ARDUINO2/ARDUINO2.ino
@@ -0,0 +1,256 @@
+#include <Wire.h>
+#include <LiquidCrystal.h>
+
+
+//declarations
+
+LiquidCrystal lcd(
+12,11,6,5,4,3);
+
+volatile int coins = 0;
+const int coinslotPin = 2;
+const int coinHopperPin = 7;
+const int relayPin1 = 8;//coin hopper
+const int relayPin2 = 9;//lock
+const int sevenSegmentPin = 10;
+const int greenLedPinPassenger = 14;
+const int redLedPinPassenger = 15;
+const int greenLedPinDriver = 16;
+const int redLedPinDriver = 17;
+
+volatile int totalInsertedCoins = 0;
+int previousCoins = 0;
+volatile int balance = 0;
+volatile int fare = 0;
+volatile int permFare;
+volatile int change = 0;
+int coinHopperPinState = 0;
+int flag = 0;
+int lockOpen = 0;
+
+//setting up all input/output pins
+void setup()
+{
+  pinMode(coinslotPin, INPUT);
+  pinMode(coinHopperPin, INPUT);
+  pinMode(relayPin1, OUTPUT);
+  pinMode(relayPin2, OUTPUT);
+  pinMode(sevenSegmentPin, OUTPUT);
+  pinMode(greenLedPinPassenger, OUTPUT);
+  pinMode(redLedPinPassenger, OUTPUT);
+  pinMode(greenLedPinDriver, OUTPUT);
+  pinMode(redLedPinDriver, OUTPUT);
+
+
+  digitalWrite(relayPin1, HIGH);
+  digitalWrite(relayPin2, HIGH);
+
+  digitalWrite(greenLedPinPassenger,LOW);
+  digitalWrite(sevenSegmentPin, HIGH);
+
+
+  //start of the recieving event
+  Wire.begin(8);
+  Wire.onReceive(receiveEvent);
+  
+  pinMode(coinslotPin,INPUT_PULLUP);
+  attachInterrupt(0, coinsInserted, FALLING);
+  
+  lcd.begin(16,2);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(" PRESS TYPE OF");
+  lcd.setCursor(0,1);
+  lcd.print("   PASSENGER");
+  
+  Serial.begin(9600);
+}
+void coinsInserted()//method in calculating the change and the coins inserted
+{
+  totalInsertedCoins = totalInsertedCoins + 1;
+  fare = fare - 1;
+  change = change + 1;
+  lcd.setCursor(0,1);
+  lcd.print("Change: ");
+  lcd.print(change);
+  lcd.print(".00 Php");
+
+}
+
+void coinHopper(int fareParam,int totalInsertedCoinsParam)//method in dispensing coins
+{
+  balance = fareParam - totalInsertedCoinsParam;
+
+  int prev = 0;
+
+  while(fare < 0)
+  {
+    coinHopperPinState = digitalRead(coinHopperPin);
+
+    digitalWrite(relayPin1, LOW);
+    if(coinHopperPinState == HIGH && coinHopperPinState != prev)
+    {
+      fare = fare + 1;
+
+    }
+    prev = coinHopperPinState;
+  }
+  digitalWrite(relayPin1, HIGH);
+}
+void openLock()//method for opening the lock
+{
+  lcd.clear();
+  lcd.print("LOCK IS OPEN");
+  lcd.setCursor(0,1);
+  lcd.print("PULL THE BARRIER");
+  digitalWrite(relayPin2, LOW);
+  delay(6000);
+  digitalWrite(relayPin2, HIGH);
+
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(" PRESS TYPE OF");
+  lcd.setCursor(0,1);
+  lcd.print("   PASSENGER");
+}
+
+void loop()//the infinite loop in order for the process to be continuous
+{
+  if(lockOpen){
+    lockOpen = 0;
+    openLock();
+  }
+  previousCoins = totalInsertedCoins;
+  delay(500);
+  if(previousCoins == totalInsertedCoins && totalInsertedCoins != 0)
+  { 
+    if(fare !=0 && fare <= totalInsertedCoins && flag)
+    {
+      coinHopper(fare,totalInsertedCoins);
+    }
+    if(fare == 0 && flag){
+
+      digitalWrite(greenLedPinPassenger,HIGH);
+      digitalWrite(redLedPinPassenger,LOW);
+      
+      openLock();
+      digitalWrite(sevenSegmentPin,LOW);
+      digitalWrite(sevenSegmentPin,HIGH);
+      digitalWrite(greenLedPinPassenger,LOW);
+      digitalWrite(greenLedPinDriver, LOW);
+      digitalWrite(redLedPinDriver, LOW);
+      fare = 0;
+      totalInsertedCoins = 0;
+      flag=0;
+      lcd.clear();
+      lcd.print(" PRESS TYPE OF");
+      lcd.setCursor(0,1);
+      lcd.print("   PASSENGER");
+    }
+  }
+}
+
+void receiveEvent(int howMany)//method in recieving the signals from arduino 1
+{
+  int data;
+
+  lcd.clear();
+  while(1 < Wire.available())
+  {
+    int data = Wire.read();
+
+  }
+  data = Wire.read();
+  Serial.println(data);
+  if(data == 1)
+  {
+    digitalWrite(redLedPinPassenger, HIGH);
+    digitalWrite(redLedPinDriver, LOW);
+    digitalWrite(greenLedPinDriver, LOW);
+
+    fare = 6;
+    permFare = 6;
+    flag =1;
+    change = -6;
+
+    lcd.clear();
+    lcd.print("Fare: ");
+    lcd.print(permFare);
+    lcd.print(".00 Php");
+
+  }
+  else if (data == 2)
+  {
+    digitalWrite(redLedPinPassenger, HIGH);
+    digitalWrite(redLedPinDriver, LOW);
+    digitalWrite(greenLedPinDriver, LOW);
+    permFare = 6;
+    fare = 6;
+    flag =1;
+    change = -6;
+
+    lcd.clear();
+    lcd.print("Fare: ");
+    lcd.print(permFare);
+    lcd.print(".00 Php");
+  }
+  else if (data == 3)
+  {
+    digitalWrite(redLedPinPassenger, HIGH);
+    digitalWrite(redLedPinDriver, LOW);
+    digitalWrite(greenLedPinDriver, LOW);
+
+    permFare = 7;
+    fare = 7;
+    flag = 1;
+    lcd.clear();
+    lcd.print("Fare:");
+    lcd.print(permFare);
+    lcd.print(".00 Php");
+    change = -7;
+  }
+  else if (data == 4)
+  {
+    lockOpen = 1;
+  }
+  else if (data == 5)
+  {
+    lcd.print(" PRESS TYPE OF");
+    lcd.setCursor(0,1);
+    lcd.print("   PASSENGER");
+
+  }
+  else if (data == 6)
+  {
+    digitalWrite(greenLedPinDriver,HIGH);
+    digitalWrite(redLedPinDriver,HIGH);
+    lcd.clear();
+    lcd.print("Waiting for ");
+    lcd.setCursor(0,1);
+    lcd.print("confirmation ...");
+
+
+  }
+  else if (data == 7)
+  {
+    digitalWrite(greenLedPinDriver, LOW);
+    digitalWrite(redLedPinDriver, LOW);
+    digitalWrite(greenLedPinPassenger, LOW);
+    digitalWrite(redLedPinPassenger, LOW);
+    lcd.clear();
+    lcd.print("Waiting to");
+    lcd.setCursor(0,1);
+    lcd.print("Open ...");
+  }
+  else if (data == 8)
+  {
+  //null
+  }
+}
+
+
+
+
+
+
+
